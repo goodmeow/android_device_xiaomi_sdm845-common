c8cc77b1e4a0e6507f24de7a2d7542070783f83e
Mesquita <mickaelmendes50@gmail.com>
2020-06-11 03:11:20 +0000

    sdm845-common: power: Switch to CAF PowerHAL
    Change-Id: Ia106eb1c1fae4c24bc6b16ec1dbf61b53c941eac

--- sdm845.mk
+++ sdm845.mk
@@ -269,10 +269,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sdm845
+    android.hardware.power@1.2-impl \
+    android.hardware.power@1.2-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 
 PRODUCT_COPY_FILES += \

af0edf6a5014b5cd7b5ef6e73ef6785050b42084
Andrey Sprynga <imolo4nik@gmail.com>
2020-06-23 02:03:35 +0700

    sdm845-common: Back # QTI to sdm845.mk
    Change-Id: I657b42fb46c1e8f1907b5ef2acf3ee81326a46cf

--- sdm845.mk
+++ sdm845.mk
@@ -275,6 +275,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml
 
+# QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     av \
     perf \
